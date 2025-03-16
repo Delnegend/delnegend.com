c946e2e5801144cda27876da78c8231010431764
Delnegend <kiennguyen19323@gmail.com>
2025-03-16 21:27:52 +0700

    feat(`components`): `ExtLink`, `RegularTitle`

---
+++ src/components/RegularTitle.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { cn } from "~/utils/utils";
+
+const props = withDefaults(defineProps<{
+	class?: string;
+}>(), {
+	class: "",
+});
+</script>
+
+<template>
+	<div :class="cn('text-2xl font-thin', props.class)">
+		<slot />
+	</div>
+</template>
