d8c9c9486246626d89eb390632b7e368b7df8f54
Delnegend <kiennguyen19323@gmail.com>
2025-04-25 19:31:11 +0700

    feat(`comp/ProjectCard`): mv details and long desc inside hover card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,53 +1,74 @@
 <script lang="ts" setup>
 import { ExternalLink } from "lucide-vue-next";
-
-import { cn } from "~/utils/utils";
-
+import { cn, type Project } from "~/utils/utils";
 import Badge from "./ui/Badge.vue";
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card";
 
 const props = withDefaults(defineProps<{
-	title: string;
-	description: string;
-	url?: string;
+	project: Project;
 	class?: string;
-	badges?: string[];
 }>(), {
 	url: undefined,
 	class: undefined,
-	badges: () => [] as string[],
 });
+
 </script>
 
 <template>
-	<Card :class="cn('bg-transparent', props.class)">
-		<CardHeader>
-			<CardTitle>
-				<a
-					v-if="props.url"
-					class="inline-flex items-center gap-1 no-underline"
-					:href="props.url"
-					target="_blank">
-					{{ props.title }}
-					<ExternalLink :size="14" />
-				</a>
-				<span v-else>{{ props.title }}</span>
-			</CardTitle>
-			<!-- eslint-disable-next-line vue/no-v-html, vue/no-v-text-v-html-on-component -->
-			<CardDescription>{{ props.description }}</CardDescription>
-		</CardHeader>
-		<CardFooter
-			v-if="props.badges.length"
-			class="flex flex-wrap gap-2">
-			<Badge
-				v-for="badge in props.badges"
-				:key="badge"
-				variant="outline"
-				:class="cn('transition-colors', 'hover:[background:var(--bg)] hover:text-[--text] text-muted-foreground', badge)">
-				{{ badge }}
-			</Badge>
-		</CardFooter>
-	</Card>
+	<HoverCard
+		:open-delay="0"
+		:close-delay="0">
+		<HoverCardTrigger>
+			<Card :class="cn('bg-transparent h-full', props.class)">
+				<CardHeader>
+					<CardTitle class="text-xl font-normal">
+						<a
+							v-if="props.project.url"
+							class="inline-flex items-center gap-1 no-underline"
+							:href="props.project.url"
+							target="_blank">
+							{{ props.project.title }}
+							<ExternalLink :size="14" />
+						</a>
+						<span v-else>{{ props.project.title }}</span>
+					</CardTitle>
+					<CardDescription class="text-balance">
+						{{ props.project.summary }}
+					</CardDescription>
+				</CardHeader>
+			</Card>
+		</HoverCardTrigger>
+
+		<HoverCardContent
+			:side-offset="12"
+			class="text-balance max-w-96 w-full font-extralight text-secondary-foreground leading-7 flex flex-col gap-3">
+			<div>
+				{{ props.project.description }}
+			</div>
+			<ul
+				v-if="props.project.details"
+				class="space-y-2">
+				<li
+					v-for="detail in props.project.details"
+					:key="detail"
+					class="list-disc ml-4">
+					{{ detail }}
+				</li>
+			</ul>
+			<div
+				v-if="props.project.badges.length"
+				class="flex flex-wrap gap-2">
+				<Badge
+					v-for="badge in props.project.badges"
+					:key="badge"
+					variant="outline"
+					:class="cn('transition-colors', 'hover:[background:var(--bg)] hover:text-[--text] text-muted-foreground', badge)">
+					{{ badge }}
+				</Badge>
+			</div>
+		</HoverCardContent>
+	</HoverCard>
 </template>
 
 <style scoped>
