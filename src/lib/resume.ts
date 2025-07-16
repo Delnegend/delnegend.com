40dee6f0b204227d60c88ebb691d3452100d32b9
Delnegend <kiennguyen19323@gmail.com>
2025-07-17 00:44:45 +0700

    feat: use pages; redirect resume paths to file

---
+++ src/lib/resume.ts
@@ -0,0 +1,2 @@
+export const resumeUrl =
+	'https://drive.google.com/file/d/1GWZ_Hdvya1P0Ws9pu2nAYiyGvpNwLrLo/view?usp=sharing'
---
+++ src/pages/resume.pdf.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { useHead } from 'nuxt/app'
+import ExtLink from '~/components/ExtLink.vue'
+import { resumeUrl } from '~/lib/resume'
+
+useHead({
+	meta: [
+		{
+			'http-equiv': 'refresh',
+			content: `0; url=${resumeUrl}`
+		}
+	]
+})
+</script>
+
+<template>
+	<div class="flex min-h-dvh items-center justify-center">
+		<div class="text-2xl text-balance">
+			If you are not redirected automatically, please click
+			<ExtLink :href="resumeUrl" target="_blank" rel="noopener noreferrer"
+				>here</ExtLink
+			>
+			to view my resume.
+		</div>
+	</div>
+</template>
+
+<style scoped>
+a {
+	font-size: var(--text-2xl);
+	font-weight: var(--font-weight-bold);
+}
+</style>
