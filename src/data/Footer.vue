ed53973eb4f7c0f0f7fc4a5dabb325ac62573f6e
Delnegend <kiennguyen19323@gmail.com>
2025-07-17 18:02:09 +0700

    refactor(`Footer`): cleaner

--- src/data/Footer.vue
+++ src/data/Footer.vue
@@ -4,24 +4,7 @@ import ExtLink from '~/components/ExtLink.vue'
 
 <template>
 	<footer class="text-center text-balance">
-		From
-		<div class="inline-block align-sub">
-			<svg
-				width="30"
-				height="20"
-				viewBox="0 0 30 20"
-				xmlns="http://www.w3.org/2000/svg"
-				version="1.1"
-			>
-				<rect width="30" height="20" fill="#da251d" />
-				<polygon
-					points="15,4 11.47,14.85 20.71,8.15 9.29,8.15 18.53,14.85"
-					fill="#ff0"
-				/>
-			</svg>
-		</div>
-		<span class="text-xl font-bold"> Vietnam </span> with love. Design
-		inspired by
+		Design inspired by
 		<ExtLink href="https://excalidraw.com/">Excalidraw</ExtLink>
 	</footer>
 </template>
