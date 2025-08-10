959670070dc6e7399d8691856c794cffb3d1e1db
Delnegend <kiennguyen19323@gmail.com>
2025-08-10 11:52:21 +0700

    build(`postinstall`): add `pnpm i`

--- .devcontainer/postinstall.sh
+++ .devcontainer/postinstall.sh
@@ -9,3 +9,4 @@ export VOLTA_HOME="$HOME/.volta" && export PATH="$VOLTA_HOME/bin:$PATH"
 echo 'export VOLTA_HOME="$HOME/.volta"' >> ~/.zshrc
 echo 'export PATH="$VOLTA_HOME/bin:$PATH"' >> ~/.zshrc
 volta install node@lts pnpm && pnpm config set store-dir ~/.pnpm-store
+pnpm i
