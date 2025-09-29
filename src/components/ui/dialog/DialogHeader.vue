49997d62b77993430e370012c3b27dbbd1fd633d
Delnegend <kiennguyen19323@gmail.com>
2025-09-30 00:24:47 +0700

    refactor(`DialogHeader`): include the close button

--- src/components/ui/dialog/DialogHeader.vue
+++ src/components/ui/dialog/DialogHeader.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import DialogClose from './DialogClose.vue'
+import { X } from 'lucide-vue-next'
 import type { HTMLAttributes } from 'vue'
 import { cn } from '~/lib/utils'
 
@@ -10,8 +12,19 @@ const props = defineProps<{
 <template>
 	<div
 		data-slot="dialog-header"
-		:class="cn('flex flex-col gap-2 text-center sm:text-left', props.class)"
+		:class="
+			cn(
+				'items-center flex flex-row gap-2 text-center justify-between',
+				props.class
+			)
+		"
 	>
 		<slot />
+		<DialogClose
+			class="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-8 cursor-pointer"
+		>
+			<X />
+			<span class="sr-only">Close</span>
+		</DialogClose>
 	</div>
 </template>
