11ade5be6ecc673879f558584d135c439f1fa501
Delnegend <kiennguyen19323@gmail.com>
2025-09-30 00:25:22 +0700

    refactor(`data/links`): only keep service name & link

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,22 +1,8 @@
 export const links = {
-	Discord: {
-		value: '@delnegend',
-		link: 'https://discordapp.com/users/577800591724642304'
-	},
-	// Steam: {
-	// 	value: 'Kien',
-	// 	link: 'https://steamcommunity.com/id/delnegend'
-	// },
-	GitHub: {
-		value: 'Delnegend',
-		link: 'https://github.com/Delnegend'
-	},
-	Email: {
-		value: 'kien@delnegend.com',
-		link: 'mailto:kien@delnegend.com'
-	}
-	// Watchlist: {
-	// 	value: 'anime/movie/series',
-	// 	link: 'https://delnegend.notion.site/Movie-list-cd5e30f30db14ffebf9898d69a823d8c?source=copy_link'
-	// }
+	GitHub: 'https://github.com/Delnegend',
+	Discord: 'https://discordapp.com/users/577800591724642304',
+	Steam: 'https://steamcommunity.com/id/delnegend',
+	Email: 'mailto:kien@delnegend.com',
+	Watchlist:
+		'https://delnegend.notion.site/Movie-list-cd5e30f30db14ffebf9898d69a823d8c?source=copy_link'
 } as const
